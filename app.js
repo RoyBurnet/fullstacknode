922011a68e40ca7092ce0db5954f1cd6bb7b4cae
Randy <rrw.burnet@gmail.com>
2019-02-16 13:39:42 +0100

    feature/EXP-004-install-and-connect-mongoose add body parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -13,6 +14,9 @@ const Idea = mongoose.model('ideas');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   const title = 'Welcome'
@@ -27,6 +31,25 @@ app.get('/about', (req, res) => {
 
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add')
+});
+
+app.post('/ideas', (req, res) => {
+  let errors = [];
+  if(!req.body.title){
+    errors.push({text: 'please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text: 'please add some details'})
+  }
+  if(errors.length > 0 ){
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send('passed')
+  }
 })
 
 const port = 5000;
